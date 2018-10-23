901f12580f4b2509f18dcdc4e3578dc0f95a96a2
Alexander <acvilla@bu.edu>
2018-10-23 18:54:58 -0400

    Renamed folder to TrapezoidalControl

---
+++ TrapezoidalControl/RAM/main.d
@@ -0,0 +1,87 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/common/include/DSP28x_Project.h
+main.obj: C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/headers/include/F2802x_Device.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-c2000_18.1.2.LTS/include/assert.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-c2000_18.1.2.LTS/include/_ti_config.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-c2000_18.1.2.LTS/include/linkage.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-c2000_18.1.2.LTS/include/stdarg.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-c2000_18.1.2.LTS/include/stdbool.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-c2000_18.1.2.LTS/include/stddef.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-c2000_18.1.2.LTS/include/stdint.h
+main.obj: C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/headers/include/F2802x_Adc.h
+main.obj: C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/headers/include/F2802x_BootVars.h
+main.obj: C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/headers/include/F2802x_DevEmu.h
+main.obj: C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/headers/include/F2802x_Comp.h
+main.obj: C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/headers/include/F2802x_CpuTimers.h
+main.obj: C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/headers/include/F2802x_ECap.h
+main.obj: C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/headers/include/F2802x_EPwm.h
+main.obj: C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/headers/include/F2802x_Gpio.h
+main.obj: C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/headers/include/F2802x_I2c.h
+main.obj: C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/headers/include/F2802x_NmiIntrupt.h
+main.obj: C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/headers/include/F2802x_PieCtrl.h
+main.obj: C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/headers/include/F2802x_PieVect.h
+main.obj: C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/headers/include/F2802x_Spi.h
+main.obj: C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/headers/include/F2802x_Sci.h
+main.obj: C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/headers/include/F2802x_SysCtrl.h
+main.obj: C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/headers/include/F2802x_XIntrupt.h
+main.obj: C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/common/include/f2802x_examples.h
+main.obj: C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/common/include/f2802x_globalprototypes.h
+main.obj: C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/common/include/f2802x_epwm_defines.h
+main.obj: C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/common/include/f2802x_i2c_defines.h
+main.obj: C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/common/include/f2802x_defaultisr.h
+main.obj: C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/common/include/clk.h
+main.obj: C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/common/include/cpu.h
+main.obj: C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/common/include/pwm.h
+main.obj: C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/common/include/flash.h
+main.obj: C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/common/include/gpio.h
+main.obj: C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/common/include/pie.h
+main.obj: C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/common/include/adc.h
+main.obj: C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/common/include/pll.h
+main.obj: C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/common/include/timer.h
+main.obj: C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/common/include/wdog.h
+main.obj: ../user.h
+
+../main.c: 
+C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/common/include/DSP28x_Project.h: 
+C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/headers/include/F2802x_Device.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-c2000_18.1.2.LTS/include/assert.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-c2000_18.1.2.LTS/include/_ti_config.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-c2000_18.1.2.LTS/include/linkage.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-c2000_18.1.2.LTS/include/stdarg.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-c2000_18.1.2.LTS/include/stdbool.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-c2000_18.1.2.LTS/include/stddef.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-c2000_18.1.2.LTS/include/stdint.h: 
+C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/headers/include/F2802x_Adc.h: 
+C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/headers/include/F2802x_BootVars.h: 
+C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/headers/include/F2802x_DevEmu.h: 
+C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/headers/include/F2802x_Comp.h: 
+C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/headers/include/F2802x_CpuTimers.h: 
+C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/headers/include/F2802x_ECap.h: 
+C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/headers/include/F2802x_EPwm.h: 
+C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/headers/include/F2802x_Gpio.h: 
+C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/headers/include/F2802x_I2c.h: 
+C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/headers/include/F2802x_NmiIntrupt.h: 
+C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/headers/include/F2802x_PieCtrl.h: 
+C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/headers/include/F2802x_PieVect.h: 
+C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/headers/include/F2802x_Spi.h: 
+C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/headers/include/F2802x_Sci.h: 
+C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/headers/include/F2802x_SysCtrl.h: 
+C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/headers/include/F2802x_XIntrupt.h: 
+C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/common/include/f2802x_examples.h: 
+C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/common/include/f2802x_globalprototypes.h: 
+C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/common/include/f2802x_epwm_defines.h: 
+C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/common/include/f2802x_i2c_defines.h: 
+C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/common/include/f2802x_defaultisr.h: 
+C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/common/include/clk.h: 
+C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/common/include/cpu.h: 
+C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/common/include/pwm.h: 
+C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/common/include/flash.h: 
+C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/common/include/gpio.h: 
+C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/common/include/pie.h: 
+C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/common/include/adc.h: 
+C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/common/include/pll.h: 
+C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/common/include/timer.h: 
+C:/ti/C2000Ware_1_00_04_00_Software/device_support/f2802x/common/include/wdog.h: 
+../user.h: 
